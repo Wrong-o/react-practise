0e5f1d58bd04ae81e0932c6c66c6c73b06eb5593
Wrong-o <otto@rongedal.se>
2025-01-17 02:39:10 +0100

    Several boxes

--- react-prac/src/App.tsx
+++ react-prac/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import HelloWorld from './HelloWorld';
+import React, {useState} from 'react';
 import DraggableTextbox from "./components/DraggableTextbox";
 
 const App: React.FC = () => {
+  const [textboxes, setTextboxes] = useState<number[]>([]);
+
+  const addTextboxes = () => {
+    const newTextboxes = Array.from(
+      {length: 5},
+      (_, i) => textboxes.length + i + 1
+    );
+    setTextboxes((prev)=> [...prev, ...newTextboxes]);
+  };
   return (
     <div
     style={{
@@ -13,7 +21,25 @@ const App: React.FC = () => {
       position: "relative",
     }}
     >
-    <DraggableTextbox />
+      <button
+      onClick={addTextboxes}
+      style={{
+        position: "absolute",
+        top: "10px",
+        left: "10px",
+        padding: "10px",
+        backgroundColor: "#007bff",
+        color: "#fff",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+      }}
+      >
+      Add 5 textboxes
+      </button>
+      {textboxes.map((id) => (
+        <DraggableTextbox key={id} />
+      ))}
     </div>
   );
 };
